88f3e016abf0a2c8c82cc171a721622d39a96208
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2021-08-11 08:36:34 -0400

    Primeira Aula

--- src/main/java/paradigmaFuncional.java
+++ src/main/java/paradigmaFuncional.java
@@ -1,5 +1,9 @@
+import java.util.function.UnaryOperator;
+
 public class paradigmaFuncional {
     public static void main(String[] args) {
-        
+        UnaryOperator<Integer> calcularValorVezesTres = valor -> valor*3;
+        int valor = 10;
+        System.out.println("O resultado é :: " + calcularValorVezesTres.apply(valor));
     }
 }
